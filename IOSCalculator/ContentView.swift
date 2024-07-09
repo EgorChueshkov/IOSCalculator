612c8e6998416989aaf816c5e48e89cddd3e97f3
EgorChueshkov <egor.chueshkov@yandex.by>
2024-07-09 20:32:52 +0300

    add project IOSCalculator

---
+++ IOSCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSCalculator
+//
+//  Created by Egor on 9.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSCalculator/IOSCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSCalculatorApp.swift
+//  IOSCalculator
+//
+//  Created by Egor on 9.07.24.
+//
+
+import SwiftUI
+
+@main
+struct IOSCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
